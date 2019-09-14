3de3f973370c85e6f366f897ce6bf7de619bccd7
Faizan Khan <faizank575@gmail.com>
2019-09-14 21:51:20 +0500

    updated main()

--- app.py
+++ app.py
@@ -59,4 +59,9 @@ def image_upload():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
